41b603411604e78f67d244e54166a35a76965365
GianniBuoni <162598175+GianniBuoni@users.noreply.github.com>
2026-02-03 14:45:33 -0500

    feat: add dendritic template

---
+++ den/flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "GianniBuoni's dendritic nix bootstrapper.";
+
+  outputs = inputs: inputs.flake-parts.lib.mkFlake {inherit inputs;} (inputs.import-tree ./modules);
+
+  inputs = {
+    # core
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    import-tree.url = "github:vic/import-tree";
+    # dentrtic
+    flake-aspects.url = "github:vic/flake-aspects";
+    flake-file.url = "github:vic/flake-file";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    # systems
+    systems = {
+      url = "github:nix-systems/default";
+      flake = false;
+    };
+  };
+}
